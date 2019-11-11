c4119471c6fdd83edaf52c8cf0ca11e91cd3bee1
colinstulp <57618732+colinstulp@users.noreply.github.com>
2019-11-11 20:46:23 +0100

    Test

---
+++ Colin-test/include/header.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <link rel="stylesheet" type="text/css" href="PATHTOCSSHERE">.
+        <li><a href="">Link 1</a></li>
+        <li><a href="">Link 2</a></li>
+        <li><a href="">Link 3</a></li>
+        <li><a href="">Link 4</a></li>
+    </ul>
+</nav>
---
+++ Colin-test/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="description" content="Your meta description"/>
+    <title>My Page</title>
+    <link rel="stylesheet" type="text/css" href="/include/bootstrap.css"/>
+</head>
+<body>
+<header>
+    <?php
+    include 'include/header.php';
+    ?>
+</header>
+<div class="container" type="">
+    <h1>Heading 1</h1>
+    <p>Some text.</p>
+    <h2>Heading 2</h2>
+    <p>Some more text.</p>
+</div>
+<footer>
+    <?php
+    include 'include/footer.php';
+    ?>
+</footer>
+</body>
+</html>
