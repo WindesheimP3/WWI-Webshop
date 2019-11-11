0cd74485454c097a17b3dcb37ea3c4226f9f3cd6
TheSecondSpoon <57615825+TheSecondSpoon@users.noreply.github.com>
2019-11-11 14:18:10 +0100

    Test

--- Test.php
+++ Test.php
@@ -1,17 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Voorpagina</title>
-</head>
-<body>
-<nav class="nav justify-content-center">
-    <a class="nav-link active" href="#">Active</a>
-    <a class="nav-link" href="#">Link</a>
-    <a class="nav-link" href="#">Link</a>
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-</nav>
-</body>
-</html>
+
+<?php
+include '/inc/header.php';
+?>
+    <h1>Heading 1</h1>
+    <p>Some text.</p>
+    <h2>Heading 2</h2>
+    <p>Some more text.</p>
+<?php
+include '/inc/footer.php';
+?>
