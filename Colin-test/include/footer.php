2ed6f78a4b26fdf59697670515c2678432737661
colinstulp <57618732+colinstulp@users.noreply.github.com>
2019-11-11 21:18:20 +0100

    index update

--- Colin-test/include/footer.php
+++ Colin-test/include/footer.php
@@ -1,6 +1,7 @@
-<ul>
-    <li><a href="">Link 1</a></li>
-    <li><a href="">Link 2</a></li>
-    <li><a href="">Link 3</a></li>
-    <li><a href="">Link 4</a></li>
-</ul>
+<nav class="navbar fixed-bottom">
+    <a class="navbar-brand col-1 text-left small" href="">Over ons</a>
+    <a class="navbar-brand col-1 text-left small" href="">Werken bij </a>
+    <a class="navbar-brand col-1 text-left small" href="">Klantenservice</a>
+    <a class="navbar-brand col-1 text-left small" href="">FAQ</a>
+    <a class="navbar-brand col-7 text-right small"> &copyWindesheim P3</a>
+</nav>
--- Colin-test/include/header.php
+++ Colin-test/include/header.php
@@ -1,9 +1,13 @@
-<nav>
-    <ul>
-        <link rel="stylesheet" type="text/css" href="PATHTOCSSHERE">.
-        <li><a href="">Link 1</a></li>
-        <li><a href="">Link 2</a></li>
-        <li><a href="">Link 3</a></li>
-        <li><a href="">Link 4</a></li>
-    </ul>
+
+<nav class="nav justify-content-center">
+    <a class="nav-brand col-10 h1">WEBSHOPNAAM</a>
+    <a class="nav-link col-1">Verlanglijst</a>
+    <a class="nav-link col-1 small">Mijn profiel</a>
+    <a class="nav-link" href="#">Home</a>
+    <a class="nav-link" href="#">Pagina 1</a>
+    <a class="nav-link" href="#">Pagina 2</a>
+    <a class="nav-link" href="#">Pagina 3</a>
+    <a class="nav-link" href="#">Aanbieding</a>
+    <a class="nav-link" href="#">Search</a>
+    <a class="nav-link" href="#">Winkelmand</a>
 </nav>
