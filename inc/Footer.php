c3d80a8601bbdb2babdc438b69c5ebf718340457
TheSecondSpoon <57615825+TheSecondSpoon@users.noreply.github.com>
2019-11-12 09:27:38 +0100

    Verbetering

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,10 +1,10 @@
 </div>
 <footer>
-    <nav class="navbar fixed-bottom-bottom">
-        <a class="nav-link col-1 text-left small" href="">Over ons</a>
-        <a class="nav-link col-1 text-left small" href="">Werken bij </a>
-        <a class="nav-link col-1 text-left small" href="">Klantenservice</a>
-        <a class="nav-link col-1 text-left small" href="">FAQ</a>
+    <nav class="navbar fixed-bottom">
+        <a class="nav-link col-1 text-left" href="">Over ons</a>
+        <a class="nav-link col-1 text-left" href="">Werken bij </a>
+        <a class="nav-link col-1 text-left" href="">Klantenservice</a>
+        <a class="nav-link col-1 text-left" href="">FAQ</a>
         <a class="nav-link col-7 text-right"> &copyWindesheim P3</a>
     </nav>
 </footer>
