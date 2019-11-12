2ffa9fbfce40fd8ab7097f5933dedeaf1afcdd62
TheSecondSpoon <57615825+TheSecondSpoon@users.noreply.github.com>
2019-11-12 09:46:36 +0100

    Links toegevoegd aan footer

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,10 +1,10 @@
 </div>
 <footer>
     <nav class="navbar fixed-bottom">
-        <a class="nav-link col-1 text-left" href="">Over ons</a>
-        <a class="nav-link col-1 text-left" href="">Werken bij </a>
-        <a class="nav-link col-1 text-left" href="">Klantenservice</a>
-        <a class="nav-link col-1 text-left" href="">FAQ</a>
+        <a class="nav-link col-1 text-left" href="over-ons.php">Over ons</a>
+        <a class="nav-link col-1 text-left" href="werken-bij.php">Werken bij </a>
+        <a class="nav-link col-1 text-left" href="klantenservice.php">Klantenservice</a>
+        <a class="nav-link col-1 text-left" href="FAQ.php">FAQ</a>
         <a class="nav-link col-7 text-right"> &copyWindesheim P3</a>
     </nav>
 </footer>
