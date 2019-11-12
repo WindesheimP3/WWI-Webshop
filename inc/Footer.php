0e71c225a540deb182f4086d56c3eea6582a41b7
TheSecondSpoon <57615825+TheSecondSpoon@users.noreply.github.com>
2019-11-12 09:26:26 +0100

    Update Footer.php

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,11 +1,11 @@
 </div>
 <footer>
-    <nav class="navbar fixed-bottom">
-        <a class="navbar-brand col-1 text-left small" href="">Over ons</a>
-        <a class="navbar-brand col-1 text-left small" href="">Werken bij </a>
-        <a class="navbar-brand col-1 text-left small" href="">Klantenservice</a>
-        <a class="navbar-brand col-1 text-left small" href="">FAQ</a>
-        <a class="navbar-brand col-7 text-right small"> &copyWindesheim P3</a>
+    <nav class="navbar fixed-bottom-bottom">
+        <a class="nav-link col-1 text-left small" href="">Over ons</a>
+        <a class="nav-link col-1 text-left small" href="">Werken bij </a>
+        <a class="nav-link col-1 text-left small" href="">Klantenservice</a>
+        <a class="nav-link col-1 text-left small" href="">FAQ</a>
+        <a class="nav-link col-7 text-right"> &copyWindesheim P3</a>
     </nav>
 </footer>
 </body>
