bcd97904cf4f00e7ee640b2bb493b7ed6ef94e37
Nogikle <57618423+Nogikle@users.noreply.github.com>
2019-11-12 12:02:18 +0100

    Update Search.php

--- inc/Search.php
+++ inc/Search.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
-    <title>Webshopnaam</title>
-    <link rel="stylesheet" type="text/css" href="bootstrap.css">
-</head>
-<p><body>
 <form method="post" action="search.php?go"  id="searchform">
     <input type="text" name="name">
     <input  type="submit" name="submit" value="Search">
@@ -13,16 +5,22 @@
 <?php
 if(isset($_POST['submit'])){ //kijken of het gesubmit kan worden
     if(isset($_GET['go'])){ //kijken of bij de "if" hij met de database matcht
-        if(preg_match("^/[A-Za-z]+/", $_POST['name'])){
+        if(preg_match("/^([A-Za-z]+)/", $_POST['name'])){
             $name=$_POST['name']; //volgende verbinden met database
-            include ("database-connectie.php");
-            $sql = "SELECT StockItemName, StockItemID, FROM wideworldimporters.stockitems WHERE StockItemName LIKE '%" . $name . "%' OR StockItemID LIKE '%" . $name . "%'";
-            $result = mysqli_query($sql);
+            $name1 = $name;
+            $name2 = $name;
+            $name3 = $name;
+            $database = include ("database-connectie.php");
+            $sql = "SELECT StockItemName, StockItemID, FROM wideworldimporters.stockitems WHERE StockItemName LIKE '%?%' OR StockItemID LIKE '%?%'";
+            $statement = mysqli_prepare($connection, "SELECT StockItemName, StockItemID, FROM wideworldimporters.stockitems WHERE StockItemName LIKE '%?%' OR StockItemID LIKE '%?%'");
+
+            mysqli_stmt_bind_param($statement, 'sss', $name1, $name2, $name3);
+            mysqli_stmt_execute($statement);
+            $result = mysqli_stmt_get_result($statement);
             while ($row=mysqli_fetch_array($result)){
                 $SIname = $row['StockItemName'];
                 $SIID = $row['StockItemID'];
-                print ("<ul>""\n");
-                print "<li>" . "<a  href=\"search.php?id=$SIname\">"   .$SIID . "</a></li>" "\n";
+                print "<li>" . "<a  href=\"search.php?id=$SIname\">"   .$SIID . "</a></li>";
                 print ("</ul>");
                 }
             }
@@ -30,8 +28,5 @@ if(isset($_POST['submit'])){ //kijken of het gesubmit kan worden
     } else{
         print ("<p>Please enter a search query</p>");
     }
-}
 ?>
-</body>
-</html>
-</p>
+
