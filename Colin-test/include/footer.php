d1e9a2fe7f58862a952a982c8945afa7db2540fe
colinstulp <57618732+colinstulp@users.noreply.github.com>
2019-11-12 09:54:11 +0100

    doe

--- Colin-test/include/footer.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar fixed-bottom">
-    <a class="navbar-brand col-1 text-left small" href="">Over ons</a>
-    <a class="navbar-brand col-1 text-left small" href="">Werken bij </a>
-    <a class="navbar-brand col-1 text-left small" href="">Klantenservice</a>
-    <a class="navbar-brand col-1 text-left small" href="">FAQ</a>
-    <a class="navbar-brand col-7 text-right small"> &copyWindesheim P3</a>
-</nav>
