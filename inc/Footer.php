df5db9716d3e97e8630ec763d186dfef7bd5bb7b
Nogikle <57618423+Nogikle@users.noreply.github.com>
2019-11-18 11:32:37 +0100

    Update Footer.php

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,12 +1,19 @@
+<br>
+<br>
+<br>
 </div>
 <footer>
     <!-- Onderste navigatiebar met links naar betreffende pagina -->
-    <nav class="navbar" id="Footer">
+    <nav class="navbar fixed-bottom" id="Footer">
+        <a class="col-3"></a>
         <a class="nav-link col-1 text-left" href="over-ons.php">Over ons</a>
+        <a class="col-1"></a>
         <a class="nav-link col-1 text-left" href="werken-bij.php">Werken bij </a>
+        <a class="col-1"></a>
         <a class="nav-link col-1 text-left" href="klantenservice.php">Klantenservice</a>
+        <a class="col-1"></a>
         <a class="nav-link col-1 text-left" href="FAQ.php">FAQ</a>
-        <a class="nav-link col-7 text-right"> &copyWindesheim P3</a>
+        <a class="nav-link col-2 text-right"> &copyWindesheim P3</a>
     </nav>
 </footer>
 </body>
