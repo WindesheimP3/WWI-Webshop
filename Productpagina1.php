97c0b18822863825b08af3c45eaf81f1b2812c7f
colinstulp <57618732+colinstulp@users.noreply.github.com>
2019-11-19 12:48:17 +0100

    commit

--- Productpagina1.php
+++ Productpagina1.php
@@ -24,36 +24,58 @@ include 'inc/Header.php';
         $UnitPrice = $row["UnitPrice"];
         $MarketingComments = $row["MarketingComments"];
         $TypicalWeightPerUnit = $row["TypicalWeightPerUnit"];
+        $QuantityOnHand = $row["QuantityOnHand"];
         ?>
         <div class="container">
-            <div class="row">
+            <div class="row" style="padding-top: 10px; padding-left: 10px;">
                 <h2><?php print($ItemName) ?></h2><br>
             </div>
             <div class="row" style="padding: 10px;">
                 <div class="col-*-*">
                     <img src="img/img_lights.jpg" style="width: 500px; height: 300px;">
-                    <div class="col-*-*" style="padding-left: 0; padding-top: 10px;">
-                    <p><?php if ($row["MarketingComments"] != null) {
-                            print ("Nice to know: $MarketingComments<br>");
-                        }
-                        }
-                        ?>
-                    </p>
-                    </div>
                 </div>
-                <div class="col-*-*" style="padding: 10px;">
+                <div class="col-*-*" style="padding-left:20px; padding-top: 10px;">
                     <p>Recommended price: €<?php print($RecommendedRetailPrice) ?></p>
                     <p>Our price: €<?php print($UnitPrice) ?></p>
+                    <p>Quanity on hand: <?php print ($QuantityOnHand) ?></p>
                     <p>Weight: <?php print($TypicalWeightPerUnit) . "kg" ?></p>
                     <p>
                         <?php if ($row["Size"] != null) {
                             print ("Size: $ItemSize <br>");
                         } ?>
                     </p>
+                    <button type="button" class="btn btn-success">In basket</button>
+                </div>
+                <div class="container" style="padding: 10px;">
+                    <div class="row">
+                        <h2>Productinformation</h2>
+                    </div>
+                    <div class="row">
+                        <p><?php if ($row["MarketingComments"] != null) {
+                                print ("Nice to know: $MarketingComments<br>");
+                            }
+                            }
+                            ?>
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam cursus vitae lacus in
+                            pretium.
+                            Cras eleifend nulla et tellus aliquam, nec porttitor purus pulvinar. Vivamus eu ex
+                            fringilla, ornare est in, tincidunt tortor. Pellentesque at risus ut sapien convallis
+                            dapibus. Nunc hendrerit finibus tristique. Phasellus vitae orci sed tellus interdum
+                            posuere
+                            sit amet eu lectus. Ut at nisi et mi condimentum suscipit. Aenean a augue sagittis,
+                            commodo
+                            libero quis, sagittis dolor. Mauris lacinia finibus metus. Mauris sed ante quis eros
+                            venenatis malesuada. Praesent sit amet velit pretium, semper turpis in, maximus mi.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-                <?php
-                include "sql-statements/database-Sluit.php";
-                include 'inc/Footer.php';
-                ?>
+    </div>
+</div>
+<?php
+include "sql-statements/database-Sluit.php";
+include 'inc/Footer.php';
+?>
