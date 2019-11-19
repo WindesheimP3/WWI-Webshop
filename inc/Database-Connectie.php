862a73c5797cdcbec8bd07ab583607a9b76e9017
kunst0599 <57618821+kunst0599@users.noreply.github.com>
2019-11-19 11:43:05 +0100

    niks

--- inc/Database-Connectie.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$databasename = "wideworldimporters";
-$user = "root";
-$pass = "";
-$port = "3306";
-$connect = mysqli_connect($host, $user, $pass, $databasename,  $port);
-?>
-
