83be8a30a6958501a93e3010389bb3c4e454ffe0
TheSecondSpoon <57615825+TheSecondSpoon@users.noreply.github.com>
2019-11-21 13:39:34 +0100

    Appel

--- inc/Sidebar.php
+++ inc/Sidebar.php
@@ -5,14 +5,14 @@ include "sql-statements/sidebar/SQL-category.php"
 ?>
 <nav class="sidebar col-1.9 sidebar">
     <div class="sidebar-sticky nav flex-column" id="Sidebar">
-        <a class="nav-item h5"> Categorieën</a>
+        <a class="nav-item h5"> Categories</a>
         <br>
         <!-- link voor elke categorie geeft ook een datagegeven aan de categorie pagina om alleen een query te maken zodat alleen producten van deze categorie getoond worden -->
         <?php
         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
             $StockGroupID = $row["StockGroupID"];
             $StockGroupName = $row["StockGroupName"];
-            print(" <a class=\"nav-item\" href=\"categorie.php?StockGroupID={$StockGroupID}\">{$StockGroupName}</a><br>");
+            print(" <a id='Sidebar1' class=\"nav-item\" href=\"categorie.php?StockGroupID={$StockGroupID}\">{$StockGroupName}</a><br>");
         }
         ?>
     </div>
