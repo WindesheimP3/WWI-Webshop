85514c742c216efd721b1525f64fd39d82f88107
Nogikle <57618423+Nogikle@users.noreply.github.com>
2019-11-28 13:51:59 +0100

    password reset

--- profilepage.php
+++ profilepage.php
@@ -30,7 +30,7 @@ include 'inc/paging-zoek/paging-start.php';
         <h1>Hi, <b><?php print htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <div class="row">
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="passwordreset.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
     </div>
