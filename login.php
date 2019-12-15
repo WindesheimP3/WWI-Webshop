dd93bcacf1ff9555b066693b79e23b6c8645dcd8
Nogikle <57618423+Nogikle@users.noreply.github.com>
2019-12-15 21:44:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -72,12 +72,12 @@ include 'inc/paging-zoek/paging-start.php';
                                     header("location: profilepage.php");
                                 } else{
                                     // Display an error message if password is not valid
-                                    $password_err = "The password you entered was not valid.";
+                                    $password_err = "The username or password you entered was incorrect";
                                 }
                             }
                         } else{
                             // Display an error message if username doesn't exist
-                            $username_err = "No account found with that username.";
+                            $username_err = "The username or password you entered was incorrect";
                         }
                     } else{
                         echo "Oops! Something went wrong. Please try again later.";
@@ -107,14 +107,15 @@ include 'inc/paging-zoek/paging-start.php';
             <p>Please fill in your credentials to login.</p>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                    <span class="help-block"><?php echo $password_err; ?></span><br>
                     <label>Username</label>
                     <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                    <span class="help-block"><?php echo $username_err; ?></span>
                 </div>
-                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <div class="form-group">
                     <label>Password</label>
                     <input type="password" name="password" class="form-control">
-                    <span class="help-block"><?php echo $password_err; ?></span>
+
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Login">
